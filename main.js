a18c390c85b419375d33ca487bbe46d885ce819e
ClonaCartaoAtualizado2024 <163008911+ClonaCartaoAtualizado2024@users.noreply.github.com>
2024-08-26 10:25:14 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+
 const jump = () => {
     mario.classList.add('jump');
+
+    setTimeout(() => )
 }
 document.addEventListener('keydown', jump);
+
+
 
 
