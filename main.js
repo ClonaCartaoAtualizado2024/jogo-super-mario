468dc5f88ff432a20d25869d15ffa404b999dfbc
ClonaCartaoAtualizado2024 <163008911+ClonaCartaoAtualizado2024@users.noreply.github.com>
2024-08-28 11:23:07 -0300

    .

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ const loop = setInterval (() => {
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = "none";
+        mario.style.left = `${marioPosition}px`;
     }
 });
 
