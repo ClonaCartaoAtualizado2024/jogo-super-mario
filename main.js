9d24acdfd01995b3a4f2291a91d46ceb723afb03
ClonaCartaoAtualizado2024 <163008911+ClonaCartaoAtualizado2024@users.noreply.github.com>
2024-09-18 11:16:05 -0300

    .

--- main.js
+++ main.js
@@ -9,12 +9,14 @@ const audioStart = new Audio("sound/audio_theme.mp3");
 const gameOverSound = new Audio("sound/audio_gameover.mp3");
 
 let gameStarted = false;
+let score = 0;
 
 const startGame = () => {
     gameStarted = true;
     audioStart.play();
 
     pipe.style.animation = "pipe-animation 1.5 infinite linear";
+
     startButton.style.display = "none";
     mario.style.opacity = "1";
     pipe.style.opacity = "1";
@@ -30,6 +32,11 @@ const jump = () => {
     } 
     ,500);
     }
+}
+
+const updateScore = () => {
+    score += 1;
+    scoreElement.textContent = score;
 }
 
 const loop = setInterval (() => {
@@ -52,6 +59,9 @@ const loop = setInterval (() => {
 
         clearInterval(loop);
         gameOverScreen.style.display = "flex";
+    } else if (pipePosition < 0 && gameStarted) {
+        updateScore();
+        pipe.style.left = '';
     }
 },10);
 
